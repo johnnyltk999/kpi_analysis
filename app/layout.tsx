4e4dc215c2d7dda469429bbe0ebd2fad4f82f3dc
000110200001882\Santisouk <johnnyltk999@gmail.com>
2025-12-12 10:36:21 +0700

    latest fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,23 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Noto_Sans_Lao } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { Noto_Sans_Lao } from "next/font/google";
+import { ConfigProvider } from "antd";
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 const notoSansLao = Noto_Sans_Lao({
   weight: ["400", "700"],
   subsets: ["lao"],
-  variable: "--font-noto-sans-lao",
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -30,11 +31,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${notoSansLao.variable} antialiased`}
-      >
-        <AntdRegistry>{children}</AntdRegistry>
+    <html lang="lo">
+      <body className={notoSansLao.className}>
+        <ConfigProvider
+          theme={{
+            token: {
+              fontFamily: "Noto Sans Lao, sans-serif",
+            },
+          }}
+        >
+          <AntdRegistry>{children}</AntdRegistry>
+        </ConfigProvider>
       </body>
     </html>
   );
